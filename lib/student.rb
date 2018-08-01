acb2a50fd9fc930e3a7841ec6e890937bf90fcea
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-01 23:24:47 +0000

    automatically backed up by learn

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+class Student
+ attr_accessor :student
+
+ def initialize
+   @student
+ end
+
+  def hello
+    "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    "Pick me!"
+  end
+
+end
